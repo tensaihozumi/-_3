99b5a4de71dd642dedf6e9a5f3e9a312104b9c3a
hozumi <tvxqg5jyj@gmail.com>
2019-11-11 12:53:30 +0900

    Frame 간단하게 생성

---
+++ MyHelloFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+/**
+ * Write a description of class MyHelloFrame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyHelloFrame extends JFrame
+{
+    public MyHelloFrame(){
+        setTitle("실습_3");
+        setSize(300,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
